132542d3527e931f8572edb35123f79e78efb897
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-05-18 22:53:45 +0530

    Refactor Upcasting example with method overriding
    Updated the Upcasting example to demonstrate method overriding and runtime polymorphism.

--- src/oops_concepts/Upcasting.java
+++ src/oops_concepts/Upcasting.java
@@ -1,28 +1,55 @@
-//give example for upcasting
+// Parent class
+class Animal {
 
-class Animal{
-    void sound(){
+    // Method of parent class
+    void sound() {
         System.out.println("Animal makes a sound");
     }
-    void bark(){
+
+    // Another method of parent class
+    void bark() {
         System.out.println("Animal barks");
     }
 }
-class Dog extends Animal{
-    void sound(){
-        System.out.println("dog makes sound");
+
+// Child class inheriting Animal
+class Dog extends Animal {
+
+    // Method overriding
+    @Override
+    void sound() {
+        System.out.println("Dog makes sound");
     }
-    void run(){
-        System.out.println("dog runs fast");
+
+    // Method only available in Dog class
+    void run() {
+        System.out.println("Dog runs fast");
     }
 }
 
-public class Upcasting{
-    public static void main(String[] args){
+public class Upcasting {
+
+    public static void main(String[] args) {
+
+        // Upcasting:
+        // Parent class reference holding Child class object
         Animal puppy = new Dog();
+
+        // Normal object creation using child class reference
         Dog puppy2 = new Dog();
+
+        // Calls overridden method from Dog class
+        // Runtime polymorphism
         puppy.sound();
+
+        // Calls method from Animal class
         puppy.bark();
-        //puppy.run(); give error
+
+        // ERROR:
+        // Parent reference cannot access child-specific methods
+        // puppy.run();
+
+        // Child reference can access all child methods
+        puppy2.run();
     }
 }
