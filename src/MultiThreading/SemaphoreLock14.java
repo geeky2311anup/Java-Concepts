ea10761dde67e0afc8a09d93aa8bd23ad22b2981
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-05-23 22:26:30 +0530

    Update SemaphoreLock14.java

--- src/MultiThreading/SemaphoreLock14.java
+++ src/MultiThreading/SemaphoreLock14.java
@@ -1,69 +1,67 @@
 import java.util.concurrent.Semaphore;
-class SharedResource{
-    boolean isAvailable = false;
-    Semaphore semaphoreobj = new Semaphore(2);//2 permits indicates 2 threads can access the shared resource at a time
-    public void producer(){
-        try{
-            semaphoreobj.acquire();
-            System.out.println("Semaphore acquired by "+Thread.currentThread().getName());
+
+class SharedResource {
+    private boolean isAvailable = false;
+    private Semaphore semaphoreObj = new Semaphore(2);
+
+    public void producer() {
+        try {
+            semaphoreObj.acquire();
+            System.out.println(Thread.currentThread().getName()
+                    + " acquired permit. Available permits: "
+                    + semaphoreObj.availablePermits());
+
             isAvailable = true;
-            Thread.sleep(8000);
+            Thread.sleep(5000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName()
+                    + " releasing permit.");
+            semaphoreObj.release();
         }
-        catch(Exception e){
-            System.out.println("Exception in producer method"); 
-        }
-        finally{
-            System.out.println("Semaphore released by "+Thread.currentThread().getName());
-             semaphoreobj.release();
-        }   
     }
-    public void consumer(){
-        try{
-            semaphoreobj.acquire();
-            System.out.println("Semaphore acquired by "+ Thread.currentThread().getName());
+
+    public void consumer() {
+        try {
+            semaphoreObj.acquire();
+            System.out.println(Thread.currentThread().getName()
+                    + " acquired permit. Available permits: "
+                    + semaphoreObj.availablePermits());
+
             isAvailable = false;
-        }
-        catch(Exception e){
-            System.out.println("Exception occured");
-        }
-        finally{
-        
-            System.out.println("Semaphore released by:"+ Thread.currentThread().getName()+"after 2 sec");
-            try{
-                Thread.sleep(2000);
-            }
-            catch(Exception e){
-                System.out.println("error occured");
-            }   
-             semaphoreobj.release();
+            Thread.sleep(2000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName()
+                    + " releasing permit.");
+            semaphoreObj.release();
         }
     }
 }
-public class SemaphoreLock14 {
-    public static void main(String[] args){
-        SharedResource resObj = new SharedResource();
-        Thread t1 = new Thread(()->{
-            resObj.producer();
-        });
-         Thread t2 = new Thread(()->{
-            resObj.producer();
-        });
-        //SharedResource resObj2 = new SharedResource();//it write lock should also be used on same shared object for best practice
-        Thread t3 = new Thread(()->{
-            resObj.consumer();
-        });
-        Thread t4 = new Thread(()->{
-            resObj.consumer();
-        });
-            Thread t5 = new Thread(()->{
-                resObj.consumer();
-            });
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+
+        SharedResource resource = new SharedResource();
+
+        // 5 producers
+        for (int i = 1; i <= 5; i++) {
+            Thread producer = new Thread(
+                    () -> resource.producer(),
+                    "Producer-" + i);
+            producer.start();
+        }
+
+        // 5 consumers
+        for (int i = 1; i <= 5; i++) {
+            Thread consumer = new Thread(
+                    () -> resource.consumer(),
+                    "Consumer-" + i);
+            consumer.start();
+        }
     }
-    
 }
