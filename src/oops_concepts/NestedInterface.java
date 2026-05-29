260b973e424c92c3773ea66cea635b957d4f46cd
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-05-29 22:55:25 +0530

    Enhance NestedInterface with new nested interfaces
    Added new nested interfaces and implemented them in classes. Updated main method to demonstrate functionality of various bird types and their behaviors.

--- src/oops_concepts/NestedInterface.java
+++ src/oops_concepts/NestedInterface.java
@@ -1,46 +1,117 @@
-interface Bird{
-    public void fly();
-    public interface NonFlyingBird{ // nested interface must have public access modifier if its inside interface
-        public void run();
+interface Bird {
+    void fly();
+
+    interface NonFlyingBird {
+        void run();
+    }
+
+    interface WaterBird {
+        void swim();
+    }
+
+    default void info() {
+        System.out.println("This is a bird.");
+    }
+
+    static void category() {
+        System.out.println("Birds belong to Aves.");
     }
 }
- class Dog{
-    public void bark(){
+
+class Dog {
+
+    public void bark() {
         System.out.println("Dog barks");
     }
-    private interface Pet{ // nested interface can have private access modifier if its inside class
-        public void play();
-    }
-    class Puppy implements Pet{
-        @Override
-        public void play(){
-            System.out.println("Puppy plays");  
-        }
- }
- }
 
-class parrot implements Bird{
+    private interface Pet {
+        void play();
+    }
+
+    static interface GuardDog {
+        void protect();
+    }
+
+    class Puppy implements Pet {
+        @Override
+        public void play() {
+            System.out.println("Puppy plays");
+        }
+    }
+
+    static class GermanShepherd implements GuardDog {
+        @Override
+        public void protect() {
+            System.out.println("German Shepherd protects the house");
+        }
+    }
+}
+
+class Parrot implements Bird {
     @Override
-    public void fly(){
+    public void fly() {
         System.out.println("Parrot can fly");
     }
 }
-class Ostrich implements Bird.NonFlyingBird{
+
+class Ostrich implements Bird.NonFlyingBird {
     @Override
-    public void run(){
+    public void run() {
         System.out.println("Ostrich can run");
     }
 }
-public class NestedInterface{
-    public static void main(String[] args){
-        parrot p = new parrot();
+
+class Duck implements Bird, Bird.WaterBird {
+
+    @Override
+    public void fly() {
+        System.out.println("Duck can fly");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Duck can swim");
+    }
+}
+
+public class NestedInterfaceDemo {
+
+    public static void main(String[] args) {
+
+        // Nested interface implementation
+        Parrot p = new Parrot();
         p.fly();
+        p.info();
+
         Ostrich o = new Ostrich();
         o.run();
-        //o.fly();
+
+        Duck d = new Duck();
+        d.fly();
+        d.swim();
+
+        // Static interface method
+        Bird.category();
+
+        // Inner class implementing private interface
         Dog dog = new Dog();
         dog.bark();
+
         Dog.Puppy puppy = dog.new Puppy();
         puppy.play();
+
+        // Static nested class implementing nested interface
+        Dog.GermanShepherd gs = new Dog.GermanShepherd();
+        gs.protect();
+
+        // Anonymous implementation of nested interface
+        Bird.NonFlyingBird penguin = new Bird.NonFlyingBird() {
+            @Override
+            public void run() {
+                System.out.println("Penguin waddles and runs");
+            }
+        };
+
+        penguin.run();
     }
 }
