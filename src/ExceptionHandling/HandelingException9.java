7b055197d7408fc8dc52567131ea66d53e86926f
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-06-26 23:42:26 +0530

    Refactor exception handling and add method2

--- src/ExceptionHandling/HandelingException9.java
+++ src/ExceptionHandling/HandelingException9.java
@@ -1,31 +1,42 @@
+import java.io.IOException;
 import java.io.InterruptedIOException;
 
 public class HandelingException9 {
-    public static void main(String[] args){
-        {
-            try{
-                method1(-10);
-            }
-            catch(ClassNotFoundException exceptionObj){
-                System.out.println("ClassNotFoundException handled successfully.");
-            }
-            catch(InterruptedException exceptionObj){
-                System.out.println("InterruptedException handled successfully.");
-            }
 
-            
+    public static void main(String[] args) {
+
+        try {
+            method1(-10);
+        } catch (ClassNotFoundException exceptionObj) {
+            System.out.println("ClassNotFoundException handled successfully.");
+        } catch (InterruptedException exceptionObj) {
+            System.out.println("InterruptedException handled successfully.");
         }
-}
 
-public static void method1(int val) throws ClassNotFoundException, InterruptedException{
-    if(val<0){
-        throw new ClassNotFoundException();
+        try {
+            method2();
+        } catch (IOException e) {
+            System.out.println("IOException handled successfully.");
+        }
     }
-    else if(val==0){
-        throw new InterruptedException();
+
+    public static void method1(int val)
+            throws ClassNotFoundException, InterruptedException {
+
+        if (val < 0) {
+            throw new ClassNotFoundException();
+        } else if (val == 0) {
+            throw new InterruptedException();
+        } else {
+            System.out.println("No Exception");
+        }
     }
-    else{
-        System.out.println("No Exception");
+
+    public static void method2() throws IOException {
+        throw new IOException("Sample IO Exception");
     }
-}
+
+    public static void displayMessage() {
+        System.out.println("Program execution completed.");
+    }
 }
