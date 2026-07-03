0aefc916b37f103fa183b178ed765a1d8ce5b0d7
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-07-03 23:40:32 +0530

    Update GenericUsingTypeCasting.java

--- src/GenericClasses/GenericUsingTypeCasting.java
+++ src/GenericClasses/GenericUsingTypeCasting.java
@@ -1,11 +1,32 @@
-    class Print {
+class Print {
     Object value;
-    public Object getValue(){
+
+    public Object getValue() {
         return value;
     }
-    public Print(Object value){
+
+    public Print(Object value) {
         this.value = value;
     }
 }
 
-// run this in GenericUsingTypeCasting.java file
+public class GenericUsingTypeCasting {
+    public static void main(String[] args) {
+
+        Print p1 = new Print(100);
+        Print p2 = new Print("Hello World");
+        Print p3 = new Print(45.67);
+        Print p4 = new Print(true);
+
+        // Type Casting
+        int num = (Integer) p1.getValue();
+        String str = (String) p2.getValue();
+        double decimal = (Double) p3.getValue();
+        boolean flag = (Boolean) p4.getValue();
+
+        System.out.println("Integer Value : " + num);
+        System.out.println("String Value  : " + str);
+        System.out.println("Double Value  : " + decimal);
+        System.out.println("Boolean Value : " + flag);
+    }
+}
