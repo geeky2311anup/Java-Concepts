4f2e4551cc437b02de2d3fd4841df28f4c22ea23
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-07-04 22:39:12 +0530

    Refactor Animal classes and implement interfaces

--- src/oops_concepts/IsARelationship.java
+++ src/oops_concepts/IsARelationship.java
@@ -1,34 +1,142 @@
-class Animal{
+class Animal {
     private String name;
-    public void sleep(){
+
+    public Animal() {
+        name = "Animal";
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void sleep() {
         System.out.println(name + " is sleeping");
     }
 
-}
-class cat extends Animal{
-    public void meow(){
-        System.out.println("cat meows");
+    public void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    public String getName() {
+        return name;
     }
 }
-interface Pet{
-    public void play();
-  
-}
-class Horse implements Pet{
-    public void graze(){
-        System.out.println("horse grazes");
+
+class Cat extends Animal {
+
+    public Cat() {
+        super("Cat");
     }
-    public void play(){
-        System.out.println("horse plays");
+
+    public void meow() {
+        System.out.println("Cat meows");
     }
-    public Horse(){
-        
+
+    @Override
+    public void eat() {
+        System.out.println("Cat drinks milk");
     }
 }
-public class IsARelationship{
-public static void main(String[] args){
-    Pet badal = new Horse();
-    badal.play();
-    
+
+class Dog extends Animal {
+
+    public Dog() {
+        super("Dog");
+    }
+
+    public void bark() {
+        System.out.println("Dog barks");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog eats bones");
+    }
 }
+
+interface Pet {
+    void play();
+}
+
+interface WildAnimal {
+    void hunt();
+}
+
+class Horse extends Animal implements Pet {
+
+    public Horse() {
+        super("Horse");
+    }
+
+    public void graze() {
+        System.out.println("Horse grazes");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Horse plays");
+    }
+}
+
+class Lion extends Animal implements WildAnimal {
+
+    public Lion() {
+        super("Lion");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("Lion hunts deer");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Lion eats meat");
+    }
+}
+
+public class IsARelationship {
+
+    public static void main(String[] args) {
+
+        // Inheritance
+        Cat cat = new Cat();
+        cat.sleep();
+        cat.eat();
+        cat.meow();
+
+        System.out.println();
+
+        Dog dog = new Dog();
+        dog.sleep();
+        dog.eat();
+        dog.bark();
+
+        System.out.println();
+
+        // Interface
+        Pet pet = new Horse();
+        pet.play();
+
+        Horse horse = new Horse();
+        horse.sleep();
+        horse.graze();
+
+        System.out.println();
+
+        // Another interface example
+        WildAnimal wild = new Lion();
+        wild.hunt();
+
+        System.out.println();
+
+        // Polymorphism
+        Animal a1 = new Cat();
+        Animal a2 = new Dog();
+        Animal a3 = new Lion();
+
+        a1.eat();
+        a2.eat();
+        a3.eat();
+    }
 }
