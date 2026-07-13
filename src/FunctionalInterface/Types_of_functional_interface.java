3802729ad671f388d85f05c6a04ac939570d1f24
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-07-13 21:44:57 +0530

    Update Types_of_functional_interface.java

--- src/FunctionalInterface/Types_of_functional_interface.java
+++ src/FunctionalInterface/Types_of_functional_interface.java
@@ -1,57 +1,76 @@
 //there are basically 4 types of functional interfaces in java
 
 //1. Consumer Functional Interface
-//in this type of functional interface, the abstract method takes input but does not return any output
+//takes input but returns nothing
 interface ConsumerFunctionalInterface<T>{
-    public void accept(T t);
+    void accept(T t);
 }
 
 //2. Supplier Functional Interface
-//in this type of functional interface, the abstract method does not take any input but returns output
+//takes no input but returns output
 interface SupplierFunctionalInterface<R>{
-    public R giveOutput();
+    R giveOutput();
 }
 
-//3. Function functional Interface
-//in this type of functional interface, the abstract method takes input and returns output
+//3. Function Functional Interface
+//takes input and returns output
 interface FunctionFunctionalInterface<R,T>{
-    public R outputfun(T t);
+    R outputfun(T t);
 }
 
 //4. Predicate Functional Interface
-//in this type of functional interface, the abstract method takes input and returns boolean output
+//takes input and returns boolean
 interface PredicateFunctionalInterface<T>{
-    public boolean test(T t);
+    boolean test(T t);
 }
+
 class Types_of_functional_interface{
-    public static void main(String [] args){
-        //1. Consumer Functional Interface
-        
-        ConsumerFunctionalInterface<String> consumerObj = (t)->{
-            System.out.println("the input provided is: "+ t );
+    public static void main(String[] args){
+
+        //================ Consumer =================
+        ConsumerFunctionalInterface<String> consumerObj = (t) -> {
+            System.out.println("The input provided is: " + t);
         };
         consumerObj.accept("Hello Consumer");
 
-        //2. Supplier Functional Interface
-        SupplierFunctionalInterface<Double> supplierObj = ()->{
+        ConsumerFunctionalInterface<Integer> squarePrinter = (n) -> {
+            System.out.println("Square = " + (n * n));
+        };
+        squarePrinter.accept(8);
+
+        //================ Supplier =================
+        SupplierFunctionalInterface<Double> supplierObj = () -> {
             return 1.234;
         };
-        System.out.println(supplierObj.giveOutput()); 
-        //3. Function functional Interface
-        FunctionFunctionalInterface<Integer,String> FunctionObj = (s)->{
+        System.out.println("Supplier Output : " + supplierObj.giveOutput());
+
+        SupplierFunctionalInterface<String> greetingSupplier = () -> {
+            return "Welcome to Java Functional Interfaces";
+        };
+        System.out.println(greetingSupplier.giveOutput());
+
+        //================ Function =================
+        FunctionFunctionalInterface<Integer, String> functionObj = (s) -> {
             return s.length();
-
         };
-        Integer a=FunctionObj.outputfun("hello java");
-        System.out.println("length of input string is "+a);
+        Integer len = functionObj.outputfun("hello java");
+        System.out.println("Length of input string is " + len);
 
-        //4. Predicate Functional Interface
-        PredicateFunctionalInterface<Integer> predicateObj = (it)->{
-            return it%1 == 0;
+        FunctionFunctionalInterface<String, Integer> binaryConverter = (num) -> {
+            return Integer.toBinaryString(num);
         };
-        Boolean it = predicateObj.test(12345);
-        System.out.println("the input integer is " +it);
+        System.out.println("Binary of 25 = " + binaryConverter.outputfun(25));
 
+        //================ Predicate =================
+        PredicateFunctionalInterface<Integer> predicateObj = (num) -> {
+            return num % 2 == 0;
+        };
+        Boolean result = predicateObj.test(12345);
+        System.out.println("Is number even? " + result);
 
-}
+        PredicateFunctionalInterface<String> emptyCheck = (str) -> {
+            return !str.isEmpty();
+        };
+        System.out.println("String is not empty: " + emptyCheck.test("Java"));
+    }
 }
