8cd09bfc6a8ae62f5280653606c523fd836079b3
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-07-23 23:51:06 +0530

    Refactor ReflectionOfMethods to display constructors and methods

--- src/reflectionInJava/ReflectionOfMethods.java
+++ src/reflectionInJava/ReflectionOfMethods.java
@@ -1,97 +1,39 @@
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+System.out.println("\n========== Constructors ==========");
+Constructor<?>[] constructors = eagleClass.getDeclaredConstructors();
 
-class Eagle {
-    public String breed = "Golden Eagle";
-    private boolean canSwim = false;
+for (Constructor<?> constructor : constructors) {
+    System.out.println("Constructor : " + constructor.getName());
+    System.out.println("Modifiers   : " + Modifier.toString(constructor.getModifiers()));
+    System.out.println("Parameters  : " + constructor.getParameterCount());
+    System.out.println("------------------------------");
+}
 
-    public void fly() {
-        System.out.println("It can fly");
-    }
+System.out.println("\n========== Method Parameters ==========");
+for (Method method : eagleClass.getDeclaredMethods()) {
+    System.out.print(method.getName() + " -> ");
+    Class<?>[] parameterTypes = method.getParameterTypes();
 
-    public void eat() {
-        System.out.println("Eating...");
-    }
-
-    private Integer legs() {
-        return 2;
-    }
-
-    private void secret() {
-        System.out.println("Private Method Invoked");
+    if (parameterTypes.length == 0) {
+        System.out.println("No Parameters");
+    } else {
+        for (Class<?> type : parameterTypes) {
+            System.out.print(type.getSimpleName() + " ");
+        }
+        System.out.println();
     }
 }
 
-public class ReflectionOfMethods {
-    public static void main(String[] args) throws Exception {
+System.out.println("\n========== Invoke Public Method ==========");
+Method flyMethod = eagleClass.getMethod("fly");
+flyMethod.invoke(eagle);
 
-        Class<Eagle> eagleClass = Eagle.class;
+System.out.println("\n========== Class Information ==========");
+System.out.println("Class Name : " + eagleClass.getName());
+System.out.println("Simple Name: " + eagleClass.getSimpleName());
+System.out.println("Package    : " + eagleClass.getPackageName());
 
-        System.out.println("========== Public Methods ==========");
-        Method[] methods = eagleClass.getMethods();
-
-        for (Method method : methods) {
-            System.out.println("Name       : " + method.getName());
-            System.out.println("ReturnType : " + method.getReturnType().getSimpleName());
-            System.out.println("Class      : " + method.getDeclaringClass().getSimpleName());
-            System.out.println("Modifier   : " + Modifier.toString(method.getModifiers()));
-            System.out.println("------------------------------");
-        }
-
-        System.out.println("\n========== Declared Methods ==========");
-        Method[] declaredMethods = eagleClass.getDeclaredMethods();
-
-        for (Method method : declaredMethods) {
-            System.out.println(method.getName());
-        }
-
-        System.out.println("\n========== Public Fields ==========");
-        Field[] fields = eagleClass.getFields();
-
-        for (Field field : fields) {
-            System.out.println(field.getName());
-            System.out.println(field.getType().getSimpleName());
-            System.out.println("------------------------------");
-        }
-
-        System.out.println("\n========== Declared Fields ==========");
-        Field[] declaredFields = eagleClass.getDeclaredFields();
-
-        for (Field field : declaredFields) {
-            System.out.println(field.getName());
-            System.out.println(field.getType().getSimpleName());
-            System.out.println("Modifier : " + Modifier.toString(field.getModifiers()));
-            System.out.println("------------------------------");
-        }
-
-        Eagle eagle = new Eagle();
-
-        System.out.println("\n========== Invoke Private Method ==========");
-        Method privateMethod = eagleClass.getDeclaredMethod("secret");
-        privateMethod.setAccessible(true);
-        privateMethod.invoke(eagle);
-
-        Method legsMethod = eagleClass.getDeclaredMethod("legs");
-        legsMethod.setAccessible(true);
-        int legs = (Integer) legsMethod.invoke(eagle);
-        System.out.println("Legs = " + legs);
-
-        System.out.println("\n========== Access Private Field ==========");
-        Field swimField = eagleClass.getDeclaredField("canSwim");
-        swimField.setAccessible(true);
-
-        System.out.println("Before : " + swimField.getBoolean(eagle));
-
-        swimField.setBoolean(eagle, true);
-
-        System.out.println("After  : " + swimField.getBoolean(eagle));
-
-        System.out.println("\n========== Access Public Field ==========");
-        Field breedField = eagleClass.getField("breed");
-        System.out.println("Breed : " + breedField.get(eagle));
-
-        breedField.set(eagle, "Bald Eagle");
-        System.out.println("Updated Breed : " + breedField.get(eagle));
-    }
-}
+System.out.println("\n========== Check Members ==========");
+System.out.println("Has field 'breed' : "
+        + (eagleClass.getField("breed") != null));
+System.out.println("Has method 'eat' : "
+        + (eagleClass.getMethod("eat") != null));
