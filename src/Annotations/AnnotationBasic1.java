cb1deb8c10e5f098f3eaa1aea92e9778cc10873f
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-07-24 23:31:16 +0530

    Refactor Bird interface and Eagle class implementation

--- src/Annotations/AnnotationBasic1.java
+++ src/Annotations/AnnotationBasic1.java
@@ -1,17 +1,30 @@
+// Interface
 interface Bird {
-    public boolean fly();
-
+    boolean fly();
 }
-class Eagle implements Bird{
-    @Override // its annotation 
-    public boolean fly(){
+
+// Eagle class implementing Bird interface
+class Eagle implements Bird {
+
+    // @Override tells the compiler that this method
+    // is overriding a method from the Bird interface.
+    @Override
+    public boolean fly() {
         return true;
     }
-}
-public class AnnotationBasic1{
-    public static void main(String[] args) {
-        Eagle eagle = new Eagle();
-        System.out.println("Can eagle fly? " + eagle.fly());
+
+    public void sound() {
+        System.out.println("Eagle makes a screeching sound.");
     }
 }
-    
+
+// Main class
+public class AnnotationBasic1 {
+
+    public static void main(String[] args) {
+        Eagle eagle = new Eagle();
+
+        System.out.println("Can eagle fly? " + eagle.fly());
+        eagle.sound();
+    }
+}
