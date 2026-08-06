1944ce803f11227f1208268e958d303916399e77
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-08-06 23:03:23 +0530

    Update reflection.java

--- src/reflectionInJava/reflection.java
+++ src/reflectionInJava/reflection.java
@@ -1,40 +1,44 @@
-//to get perticular class Class object
-//there are three ways
-//1. using forName() method
-//2. using getClass() method
-//3. using .class syntax
+// Reflection in Java
+// To get a particular Class object, there are three ways:
+// 1. Using Class.forName()
+// 2. Using .class syntax
+// 3. Using getClass()
 
 import java.lang.reflect.Modifier;
 
-// class Bird{
-//     public void fly(){
-//         System.out.println("Bird is flying");
-//     }
-// }
-// //1st
-// Class birdClass1 = Class.forName("Bird");
-
-// //2nd
-// Class birdClass2 = Bird.class;
-
-// //3rd
-// Bird obj = new Bird();
-// Class birdClass3 = obj.getClass();
-
-final class Eagle{   //here we have taken final because we have to print it to see the getModifiers
+final class Eagle {
     public String breed;
     private boolean canSwim;
-    public void fly(){
-        System.out.println("eat");
+
+    public void fly() {
+        System.out.println("Eagle is flying");
     }
 }
 
-public class reflection{
-    public static void main(String [] args){
-        Class eagleClass = Eagle.class;
-        System.out.println(eagleClass.getName());
-        System.out.println(Modifier.toString(eagleClass.getModifiers()));
-        
+public class Reflection {
+    public static void main(String[] args) {
+
+        // Get the Class object
+        Class<?> eagleClass = Eagle.class;
+
+        // Basic information
+        System.out.println("Class Name      : " + eagleClass.getName());
+        System.out.println("Simple Name     : " + eagleClass.getSimpleName());
+        System.out.println("Package Name    : " + eagleClass.getPackageName());
+        System.out.println("Modifiers       : " + Modifier.toString(eagleClass.getModifiers()));
+
+        // Print declared fields
+        System.out.println("\nFields:");
+        var fields = eagleClass.getDeclaredFields();
+        for (var field : fields) {
+            System.out.println(field.getName() + " : " + field.getType().getSimpleName());
+        }
+
+        // Print declared methods
+        System.out.println("\nMethods:");
+        var methods = eagleClass.getDeclaredMethods();
+        for (var method : methods) {
+            System.out.println(method.getName());
+        }
     }
 }
-
