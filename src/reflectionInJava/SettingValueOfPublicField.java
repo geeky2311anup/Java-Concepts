02423698cef005002d15e3c65a0402bd9b4952c8
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-08-10 23:22:34 +0530

    Refactor pairSum method to remove unnecessary functions

--- src/reflectionInJava/SettingValueOfPublicField.java
+++ src/reflectionInJava/SettingValueOfPublicField.java
@@ -1,6 +1,5 @@
 class Solution {
-
-    private List<Integer> convertToList(ListNode head) {
+    public int pairSum(ListNode head) {
         List<Integer> nums = new ArrayList<>();
 
         while (head != null) {
@@ -8,25 +7,12 @@ class Solution {
             head = head.next;
         }
 
-        return nums;
-    }
-
-    private int pairValue(List<Integer> nums, int i, int j) {
-        return nums.get(i) + nums.get(j);
-    }
-
-    public int pairSum(ListNode head) {
-
-        List<Integer> nums = convertToList(head);
-
         int left = 0;
         int right = nums.size() - 1;
         int maxSum = 0;
 
         while (left < right) {
-
-            maxSum = Math.max(maxSum, pairValue(nums, left, right));
-
+            maxSum = Math.max(maxSum, nums.get(left) + nums.get(right));
             left++;
             right--;
         }
