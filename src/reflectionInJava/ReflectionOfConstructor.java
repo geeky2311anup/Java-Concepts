c2d28562708af5970a645db5d8e1c1f81e9ff85e
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-08-17 23:48:19 +0530

    Refactor reflection example for constructor details

--- src/reflectionInJava/ReflectionOfConstructor.java
+++ src/reflectionInJava/ReflectionOfConstructor.java
@@ -28,6 +28,11 @@ class Pet {
     public void walk() {
         System.out.println(name + " is walking");
     }
+
+    public void showDetails() {
+        System.out.println("Name : " + name);
+        System.out.println("Food : " + food);
+    }
 }
 
 public class ReflectionOfConstructor {
@@ -36,48 +41,72 @@ public class ReflectionOfConstructor {
 
         Class<?> petClass = Pet.class;
 
-        // Display all constructors
-        Constructor<?>[] constructors = petClass.getDeclaredConstructors();
+        System.out.println("Class Name : " + petClass.getName());
+        System.out.println("Class Type : " + petClass.getSimpleName());
 
-        System.out.println("===== Constructor Information =====");
+        // Get all constructors including private constructors
+        Constructor<?>[] constructors =
+                petClass.getDeclaredConstructors();
+
+        System.out.println("\n===== Constructor Information =====");
 
         for (Constructor<?> constructor : constructors) {
 
-            System.out.println("Constructor Name : " + constructor.getName());
+            System.out.println("Constructor Name : "
+                    + constructor.getName());
 
-            System.out.println("Parameter Count  : " + constructor.getParameterCount());
+            System.out.println("Parameter Count  : "
+                    + constructor.getParameterCount());
 
             System.out.print("Parameter Types  : ");
-            Class<?>[] parameters = constructor.getParameterTypes();
+
+            Class<?>[] parameters =
+                    constructor.getParameterTypes();
 
             if (parameters.length == 0) {
                 System.out.print("None");
             } else {
-                for (Class<?> p : parameters) {
-                    System.out.print(p.getSimpleName() + " ");
+                for (Class<?> type : parameters) {
+                    System.out.print(type.getSimpleName() + " ");
                 }
             }
 
             System.out.println();
 
             System.out.println("Modifier         : "
-                    + Modifier.toString(constructor.getModifiers()));
+                    + Modifier.toString(
+                            constructor.getModifiers()));
 
             System.out.println("--------------------------------");
         }
 
-        // Creating object using public constructor
-        System.out.println("\n===== Creating Object Using Reflection =====");
+        // Create object using a public constructor
+        System.out.println("\n===== Public Constructor =====");
 
         Constructor<?> publicConstructor =
-                petClass.getConstructor(String.class, String.class);
+                petClass.getConstructor(
+                        String.class,
+                        String.class);
 
-        Pet pet1 = (Pet) publicConstructor.newInstance("Tom", "Fish");
+        Pet pet1 = (Pet) publicConstructor.newInstance(
+                "Tom", "Fish");
 
         pet1.walk();
+        pet1.showDetails();
 
-        // Creating object using private constructor
-        System.out.println("\n===== Accessing Private Constructor =====");
+        // Create object using the default constructor
+        System.out.println("\n===== Default Constructor =====");
+
+        Constructor<?> defaultConstructor =
+                petClass.getConstructor();
+
+        Pet pet3 = (Pet) defaultConstructor.newInstance();
+
+        pet3.walk();
+        pet3.showDetails();
+
+        // Access private constructor
+        System.out.println("\n===== Private Constructor =====");
 
         Constructor<?> privateConstructor =
                 petClass.getDeclaredConstructor(int.class);
@@ -87,5 +116,6 @@ public class ReflectionOfConstructor {
         Pet pet2 = (Pet) privateConstructor.newInstance(101);
 
         pet2.walk();
+        pet2.showDetails();
     }
 }
