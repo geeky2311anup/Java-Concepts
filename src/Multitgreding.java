b427aeeed1ae19dc83dc1dd496a2219e4a6d3ffe
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-08-23 22:19:32 +0530

    Enhance thread output with start and finish messages

--- src/Multitgreding.java
+++ src/Multitgreding.java
@@ -1,13 +1,26 @@
 class MyTask extends Thread {
+
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " started");
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+
+        System.out.println(Thread.currentThread().getName() + " finished");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
+
         MyTask t1 = new MyTask();
         MyTask t2 = new MyTask();
+
+        t1.setName("Thread-1");
+        t2.setName("Thread-2");
 
         t1.start();
         t2.start();
