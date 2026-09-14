8cda3fb5b505c7a78c4a78ae39f47e6dfc30e2d2
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-09-14 22:39:05 +0530

    Update VariableExample.java

--- src/variables/VariableExample.java
+++ src/variables/VariableExample.java
@@ -1,16 +1,18 @@
-```java
-// More StringBuilder methods
-System.out.println("StringBuilder Content: " + sb.toString());
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int leftA = rec1[0];
+        int bottomA = rec1[1];
+        int rightA = rec1[2];
+        int topA = rec1[3];
 
-sb.append(" Programming");
-System.out.println("After Adding Text: " + sb);
+        int leftB = rec2[0];
+        int bottomB = rec2[1];
+        int rightB = rec2[2];
+        int topB = rec2[3];
 
-System.out.println("Contains 'Java': "
-        + (sb.indexOf("Java") != -1));
+        boolean horizontal = Math.max(leftA, leftB) < Math.min(rightA, rightB);
+        boolean vertical = Math.max(bottomA, bottomB) < Math.min(topA, topB);
 
-sb.setLength(5);
-System.out.println("After Set Length: " + sb);
-
-sb.append(" Language");
-System.out.println("After Adding Again: " + sb);
-```
+        return horizontal && vertical;
+    }
+}
