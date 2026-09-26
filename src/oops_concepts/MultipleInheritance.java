4dda03aacbf3defc9bb1c68cb69eaf266c2baf78
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2026-09-26 23:45:42 +0530

    Refactor MultipleInheritance to use interfaces for inheritance
    Refactored classes A and B to provide motivational messages and implemented multiple inheritance using interfaces X and Y in class C.

--- src/oops_concepts/MultipleInheritance.java
+++ src/oops_concepts/MultipleInheritance.java
@@ -1,20 +1,58 @@
-class A{
-    public void motivation(){
-        System.out.println("never give up");
+class A {
+    public void motivation() {
+        System.out.println("Never give up");
     }
 }
-class B{
-    public void motivation(){
-        System.out.println("never demotivate");
+
+class B {
+    public void motivation() {
+        System.out.println("Never get demotivated");
     }
 }
-class c extends A,B{
-    //give error because java does not support multiple inheritance using class
-    //but support multiple inheritance using interface
+
+// Java does not support multiple inheritance using classes.
+// class C extends A, B { }  // ERROR
+
+// Multiple inheritance can be achieved using interfaces.
+
+interface X {
+    void motivation();
 }
-public class MultipleInheritance{
-    public static void main(String[] args){
-        c obj = new c();
-        obj.motivation(); //give error because of ambiguity
+
+interface Y {
+    void motivation();
+}
+
+// C implements multiple interfaces
+class C implements X, Y {
+
+    // Both interfaces contain the same method.
+    // C provides its own implementation to remove ambiguity.
+    @Override
+    public void motivation() {
+        System.out.println("Keep working hard and never give up!");
+    }
+
+    public void study() {
+        System.out.println("Study every day.");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+
+        // Using class inheritance
+        A objA = new A();
+        objA.motivation();
+
+        B objB = new B();
+        objB.motivation();
+
+        System.out.println();
+
+        // Multiple inheritance using interfaces
+        C objC = new C();
+        objC.motivation();
+        objC.study();
     }
 }
